210c46b90ac98da172ad862a5d20448b5766e133
Michael Carter <michaejc@metropolia.fi>
2021-04-19 16:01:57 +0300

    test ci

--- InMotion/ViewControllers/DatePickerViewController.swift
+++ InMotion/ViewControllers/DatePickerViewController.swift
@@ -11,7 +11,7 @@ class DatePickerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("screen loaded")
+        print("screen loaded now")
     }
 
 
