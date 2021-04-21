e194fee3269528365ae02df35a1619d22918c047
Michael Carter <michaejc@metropolia.fi>
2021-04-22 00:38:46 +0300

    testing fscalendar as a swift package

--- InMotion/ViewControllers/DatePickerViewController.swift
+++ InMotion/ViewControllers/DatePickerViewController.swift
@@ -5,15 +5,24 @@
 //  Created by iosdev on 16.4.2021.
 //
 
+import FSCalendar
 import UIKit
 
-class DatePickerViewController: UIViewController {
+class DatePickerViewController: UIViewController, FSCalendarDelegate {
 
+    @IBOutlet var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        calendar.delegate = self
         print("screen loaded now")
     }
 
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        calendar.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: view.frame.size.width)
+//        view.addSubview(calendar)
+//    }
 
 }
 
