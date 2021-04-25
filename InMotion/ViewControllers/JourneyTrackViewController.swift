5cdfdd0575b0ec4b5ac3773e84b881cf2f951661
KlenJavor <patricie.suppala@gmail.com>
2021-04-25 13:52:13 +0300

    Upload data with mobile data - true

--- InMotion/ViewControllers/JourneyTrackViewController.swift
+++ InMotion/ViewControllers/JourneyTrackViewController.swift
@@ -24,6 +24,7 @@ class JourneyTrackViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         TMD.start()
+        TMD.setAllowUploadOnCellularNetwork(true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
