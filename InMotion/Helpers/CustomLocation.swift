a58bdcae6ebca33d53e2435e97e6b8fe859c9a7b
jopakka <jopakkamies@gmail.com>
2021-04-30 13:54:51 +0300

    nice

--- InMotion/Helpers/CustomLocation.swift
+++ InMotion/Helpers/CustomLocation.swift
@@ -12,17 +12,17 @@ import MOPRIMTmdSdk
 class CustomLocation {
     static let instance = CustomLocation()
     
-    func generateTripToMoprim() {
+    func generateTripToMoprim() -> TMDTask<AnyObject> {
         let start = CLLocation(latitude: 60.227528, longitude: 24.7065193)
         let end = CLLocation(latitude: 60.219879, longitude: 24.6653063)
         
-        TMDCloudApi.generateSyntheticData(withOriginLocation: start, destination: end, requestType: .bicycle, hereApiKey: "SECRET").continueWith { task in
+        return TMDCloudApi.generateSyntheticData(withOriginLocation: start, destination: end, requestType: .bicycle, hereApiKey: "SECRET").continueWith { task in
             if let error = task.error {
                 print("error: ", error.localizedDescription)
             } else if let data = task.result {
                 print("result: ", data)
             }
-            return nil
+            return task
         }
     }
 }
